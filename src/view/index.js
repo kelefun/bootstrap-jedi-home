a8e9e420fbfd3a25ec9c50dcf43515a838716395
liukaiyang <liukaiyang@souche.com>
2018-11-20 23:44:00 +0800

    css update

--- src/view/index.js
+++ src/view/index.js
@@ -1,4 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap'
-import '../assets/scss/bootstrap.custom.scss';
+import '../assets/scss/index.scss';
 console.log('Its working just fine');
